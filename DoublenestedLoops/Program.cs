8f219bf5ea0f15849e6e2146ec8b37c4fa2f9ccc
Ryan LeBlanc <ryanleb2003@gmail.com>
2023-11-06 08:58:26 -0400

    Initial commit

---
+++ DoublenestedLoops/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoublenestedLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Double Nested Loops: ");
+            Console.WriteLine("(a loop within a loop!)");
+            Console.WriteLine("------------------------");
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+
+        }
+    }
+}
